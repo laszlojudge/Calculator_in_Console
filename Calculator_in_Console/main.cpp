718ae2bf1d6779487cb720c97bf6256fceaf4872
Laszlo Biro <laszlojudge@gmail.com>
2022-06-12 13:38:38 +0200

    Initial Commit

---
+++ Calculator_in_Console/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator_in_Console
+//
+//  Created by Biro Laszlo on 12.06.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
